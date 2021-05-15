2c037efe58ca17f6d3810447ca1433dd98b2c90f
CindoddCindy <cindodcindy@gmail.com>
2021-05-15 22:15:34 +0700

    tambah folder

---
+++ orderdesign/src/main/java/com/orderdesign/orderdesign/OrderdesignApplication.java
@@ -0,0 +1,13 @@
+package com.orderdesign.orderdesign;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderdesignApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderdesignApplication.class, args);
+	}
+
+}
---
+++ orderdesign/src/test/java/com/orderdesign/orderdesign/OrderdesignApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orderdesign.orderdesign;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderdesignApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
